e820798d5afd43aecd2704df4ef4aabe2a112adb
Ashisaks <ashis.singh@stu.adamasuniversity.ac.in>
2023-02-15 16:18:35 +0530

    Initial commit

---
+++ 15-02-23.py
@@ -0,0 +1,13 @@
+# WAP TO LOAD A DATASET SELECT A FEATURE AND DISPLAY IT IN A SCATTER PLOT
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df=pd.read_csv("DATASETS/insurance.csv")
+print(df.to_markdown(index=False)) # to print the data in tabular format
+x=np.arange(1,1339)
+y=df["age"]
+plt.scatter(x,y,color="k")
+plt.savefig("file.pdf")
+plt.show()
+
+
